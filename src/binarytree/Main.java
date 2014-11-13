13d3bc89e317711eb777006fd208da9c7d456b07
jymao <jymao@ucsd.edu>
2014-11-12 22:03:59 -0800

    my first commit

--- src/binarytree/Main.java
+++ src/binarytree/Main.java
@@ -3,6 +3,7 @@ package binarytree;
 public class Main {
 
 	public static void main(String[] args) {
+		//stupid binary tree has no input
 		Node n7 = new Node(null, null, 7);
 		Node n6 = new Node(null, null, 6);
 		Node n5 = new Node(null, null, 5);
